5ed098c821f5ebba05fffbeea3fa4215275281a8
Mr-Wilson-coder <kenyanyawilson0@gmail.com>
2025-07-12 01:14:53 +0300

    commenting on postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, /* enables tailwind processing it reads tailwind.config.js file and genrate all utility classes */
+    autoprefixer: {},  /* this plugin adds vendor prefixes to CSS rules for better browser support. */
   },
 }
+
+/* When you run npm run build, Vite uses PostCSS to process your CSS */
+/* it ensures Tailwind generates the correct classes and Your CSS works across multiple browsers */
